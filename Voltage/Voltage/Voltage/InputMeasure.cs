dc7da1bbf32961ae4c2fc9c335627f4502f7eafd
zyfa2008 <zyfa2008@31de1be8-87c5-11de-bed6-6d316fbe0868>
2009-08-17 07:18:11 +0000

    Add InputMeasure usercontrol

---
+++ Voltage/Voltage/Voltage/InputMeasure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Voltage
+{
+    public partial class InputMeasure : UserControl
+    {
+        public InputMeasure()
+        {
+            InitializeComponent();
+        }
+
+        private void InputMeasure_Load(object sender, EventArgs e)
+        {
+
+        }
+        public override string Text
+        {
+            get
+            {
+                return this.textBox1.Text + "," + this.textBox2.Text + "," + this.textBox3.Text;
+            }
+            set
+            {
+                int index1 = value.IndexOf(',');
+                this.textBox1.Text = value.Substring(0, index1);
+                int index2=value.IndexOf(',',index1+1);
+                this.textBox2.Text = value.Substring(index1 + 1, index2 - index1 - 1);
+                this.textBox3.Text = value.Substring(value.LastIndexOf(',') + 1);
+            }
+        }
+    }
+}
